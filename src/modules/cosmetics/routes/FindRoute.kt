90fb4870f7e0b3d87d03b98f5fc6a7f30a9e6dd5
incxption <theincxption@gmail.com>
2020-09-10 17:03:02 +0200

    :sparkles: Allow finding cosmetics by a Dragonfly account using the auth token

--- src/modules/cosmetics/routes/FindRoute.kt
+++ src/modules/cosmetics/routes/FindRoute.kt
@@ -3,14 +3,18 @@ package modules.cosmetics.routes
 import core.*
 import io.ktor.application.*
 import io.ktor.http.*
-import modules.cosmetics.util.CosmeticsController
-import modules.cosmetics.util.Filter
+import modules.cosmetics.util.*
 
 object FindRoute : ModuleRoute("find", HttpMethod.Get) {
 
     override suspend fun Call.handleCall() {
-        val minecraftUUID = call.parameters["uuid"] ?: error("No UUID specified")
-        val cosmetics = CosmeticsController.find(Filter.new().minecraft(minecraftUUID))
+        val dragonflyUUID = call.parameters["dragonfly"]
+        val cosmetics: List<CosmeticItem> = if (dragonflyUUID != null) {
+            CosmeticsController.find(Filter.new().dragonfly(dragonflyUUID))
+        } else {
+            val minecraftUUID = (call.parameters["uuid"] ?: call.parameters["minecraft"]) ?: error("No UUID specified")
+            CosmeticsController.find(Filter.new().minecraft(minecraftUUID))
+        }
 
         json {
             "success" * true
