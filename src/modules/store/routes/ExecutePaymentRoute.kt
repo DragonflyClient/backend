d1dd90fdcb3499bf082684594ef24b625745f974
incxption <theincxption@gmail.com>
2020-09-27 14:42:14 +0200

    :art: Remove try-catch block around ExecutePaymentRoute

--- src/modules/store/routes/ExecutePaymentRoute.kt
+++ src/modules/store/routes/ExecutePaymentRoute.kt
@@ -24,42 +24,55 @@ object ExecutePaymentRoute : ModuleRoute("execute_payment", HttpMethod.Post) {
 
     private val payments = database.getCollection<Payment>("payments")
 
+    private val processing = mutableListOf<String>()
+
     override suspend fun Call.handleCall() {
         // parse request
         val payload = call.receive<JsonObject>()
         val paymentId = payload["paymentId"].asString
+        processing.add(paymentId)
+        log("Payment $paymentId is now being processed...")
 
-        // load payment data
-        val payment = payments.findOne("{ paymentId: '$paymentId' }")
-            ?: checkedError("Payment $paymentId not found")
+        try {
+            // load payment data
+            val payment = payments.findOne("{ paymentId: '$paymentId' }")
+                ?: checkedError("Payment $paymentId not found")
 
-        // validate payment state
-        if (payment.paymentState != "succeeded" && payment.paymentState != "approved")
-            checkedError("Payment didn't succeed")
-        else if (payment.executed == true)
-            checkedError("Payment has already been executed!")
+            // validate payment state
+            if (payment.paymentState != "succeeded" && payment.paymentState != "approved")
+                checkedError("Payment didn't succeed")
+            else if (payment.executed == true)
+                checkedError("Payment has already been executed!")
 
-        // load shop item
-        val payedItemId = payment.itemId
-        val shopItem = shopItems.findOne("{ id: '$payedItemId' }")
-            ?: checkedError("Shop item $payedItemId not found")
+            // load shop item
+            val payedItemId = payment.itemId
+            val shopItem = shopItems.findOne("{ id: '$payedItemId' }")
+                ?: checkedError("Shop item $payedItemId not found")
 
-        // load Dragonfly account
-        val dragonflyToken = JwtConfig.verifier.verify(payment.dragonflyToken)
-        val account = dragonflyToken.getClaim("uuid").asString()?.let { uuid -> AuthenticationManager.getByUUID(uuid) }
-            ?: checkedError("Account not found")
+            // load Dragonfly account
+            val dragonflyToken = JwtConfig.verifier.verify(payment.dragonflyToken)
+            val account = dragonflyToken.getClaim("uuid").asString()?.let { uuid -> AuthenticationManager.getByUUID(uuid) }
+                ?: checkedError("Account not found")
 
-        // insert cosmetic
-        val cosmeticId = shopItem.cosmeticId
-            ?: checkedError("Shop item doesn't contain cosmetic id!")
-        CosmeticsController.insert(
-            Filter.new().dragonfly(account.uuid),
-            CosmeticItem(cosmeticId, UUID.randomUUID().toString(), enabled = true, config = Document())
-        )
-        log("Inserted cosmetic #$cosmeticId for user ${account.username}.")
+            // insert cosmetic
+            val cosmeticId = shopItem.cosmeticId
+                ?: checkedError("Shop item doesn't contain cosmetic id!")
 
-        payment.executed = true
-        payments.updateOne(Payment::paymentId eq paymentId, payment)
-        success()
+            CosmeticsController.insert(
+                Filter.new().dragonfly(account.uuid),
+                CosmeticItem(cosmeticId, UUID.randomUUID().toString(), enabled = true, config = Document())
+            )
+
+            log("Inserted cosmetic #$cosmeticId for user ${account.username}.")
+
+            payment.executed = true
+            payments.updateOne(Payment::paymentId eq paymentId, payment)
+            success()
+        } catch (e: Throwable) {
+            throw e
+        } finally {
+            processing.remove(paymentId)
+            log("Payment $paymentId has been successfully processed.")
+        }
     }
 }
