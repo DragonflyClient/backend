2cdc67d8a353ef290cff508c8217f44a6651f12b
incxption <theincxption@gmail.com>
2020-10-17 13:10:38 +0200

    :art: Restructure TokenDocument.kt

--- src/modules/cosmetics/util/models/TokenDocument.kt
+++ src/modules/cosmetics/util/models/TokenDocument.kt
@@ -20,32 +20,38 @@ data class TokenDocument(
     /**
      * The id of the cosmetic that is unlocked with the token
      */
-    @SerializedName("cosmetic_id") val cosmeticId: Int,
+    @SerializedName("cosmetic_id")
+    val cosmeticId: Int,
 
     /**
      * When the token was created (UNIX timestamp)
      */
-    @SerializedName("created_date") val createdDate: Long,
+    @SerializedName("created_date")
+    val createdDate: Long,
 
     /**
      * The Dragonfly account that created this token
      */
-    @SerializedName("created_account") val createdAccount: String,
+    @SerializedName("created_account")
+    val createdAccount: String,
 
     /**
      * Whether the token has been redeemed
      */
-    var redeemed: Boolean = false,
+    @SerializedName("redeemed")
+    var isRedeemed: Boolean = false,
 
     /**
      * The Dragonfly account that redeemed this token
      */
-    @SerializedName("redeemed_account") var redeemedAccount: String? = null,
+    @SerializedName("redeemed_account")
+    var redeemedAccount: String? = null,
 
     /**
      * The time when the token was redeemed (UNIX timestamp)
      */
-    @SerializedName("redeemed_date") var redeemedDate: Long? = null,
+    @SerializedName("redeemed_date")
+    var redeemedDate: Long? = null,
 
     /**
      * The MongoDB object id to identify the document in the database
