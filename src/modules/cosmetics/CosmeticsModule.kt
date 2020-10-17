b5a2755170bcb5d44ba8e38dbe9a17aa29d7982d
incxption <theincxption@gmail.com>
2020-10-17 10:04:19 +0200

    :art: Remove boilerplate code

--- src/modules/cosmetics/CosmeticsModule.kt
+++ src/modules/cosmetics/CosmeticsModule.kt
@@ -1,11 +1,7 @@
 package modules.cosmetics
 
 import core.Module
-import kotlinx.coroutines.runBlocking
 import modules.cosmetics.routes.*
-import modules.cosmetics.util.*
-import org.bson.Document
-import java.util.*
 
 object CosmeticsModule : Module(
     "Cosmetics",
@@ -15,16 +11,3 @@ object CosmeticsModule : Module(
     BindRoute,
     ConfigureRoute
 )
-
-fun main() = runBlocking {
-    CosmeticsController.insert(
-        Filter.new().minecraft("bc73becb-bc3b-487b-95a2-35ab00e11d0a"),
-        CosmeticItem(
-            cosmeticId = 1,
-            enabled = true,
-            config = Document().append("color", "hello"),
-            minecraft = "bc73becb-bc3b-487b-95a2-35ab00e11d0a",
-            cosmeticQualifier = UUID.randomUUID().toString()
-        )
-    )
-}
