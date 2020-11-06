e38a1bae6a7d53ae5573c4960a6cdf9c98cf1389
incxption <theincxption@gmail.com>
2020-11-06 14:22:49 +0100

    :sparkles: Introduce referral core concepts

---
+++ src/modules/partner/referrals/ReferralManager.kt
@@ -0,0 +1,46 @@
+package modules.partner.referrals
+
+import com.mongodb.client.model.UpdateOptions
+import com.mongodb.client.model.Updates
+import core.MongoDB
+import modules.partner.partners.Partner
+import org.litote.kmongo.eq
+import org.litote.kmongo.path
+
+/**
+ * Manages the granting of referral credits to Dragonfly partners.
+ */
+object ReferralManager {
+
+    /**
+     * The collection in which the referral information is stored.
+     */
+    val collection = MongoDB.partnerDB.getCollection<Referral>("referrals")
+
+    /**
+     * Returns the referral document for the given [partnerId].
+     */
+    suspend fun getDocument(partnerId: String) = collection.findOne(Referral::partnerId eq partnerId)
+
+    /**
+     * Adds the given [credit] to the partner specified by the [partnerId] in the [referralField].
+     */
+    suspend fun addCredit(partnerId: String, referralField: ReferralField, credit: Int) {
+        val detailsPath = Referral::insights.path() + "." + referralField.path
+
+        collection.updateOne(
+            Referral::partnerId eq partnerId,
+            Updates.combine(
+                Updates.inc(Referral::creditInCents.path(), credit),
+                Updates.inc("$detailsPath.${ReferralInsightsDetails::creditInCents.path()}", credit),
+                Updates.inc("$detailsPath.${ReferralInsightsDetails::timesReceived.path()}", 1)
+            ),
+            UpdateOptions().upsert(true)
+        )
+    }
+
+    /**
+     * Convenient function for calling [addCredit].
+     */
+    suspend fun Partner.addCredit(field: ReferralField, credit: Int) = addCredit(partnerId.toHexString(), field, credit)
+}
---
+++ src/modules/partner/referrals/ReferralModels.kt
@@ -0,0 +1,66 @@
+package modules.partner.referrals
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+/**
+ * Holds information about the referral bonus of a partner.
+ */
+data class Referral(
+
+    /**
+     * The id of the partner that this document belongs to.
+     */
+    @JsonProperty("partner_id")
+    val partnerId: String,
+
+    /**
+     * The total amount of credit in cents that this partner has earned.
+     */
+    @JsonProperty("credit_in_cents")
+    var creditInCents: Int = 0,
+
+    /**
+     * Insights into the different fields of referral to show how the partner earned
+     * his [creditInCents].
+     */
+    val insights: ReferralInsights = ReferralInsights()
+)
+
+/**
+ * Contains insights about how much credit the partner earned in which referral fields.
+ */
+data class ReferralInsights(
+
+    /**
+     * Details about how many accounts have been created with the referral link of the partner.
+     */
+    @JsonProperty("create_accounts")
+    var createAccounts: ReferralInsightsDetails = ReferralInsightsDetails()
+)
+
+/**
+ * Contains the details for a specific referral field.
+ */
+data class ReferralInsightsDetails(
+
+    /**
+     * How often credit has been added in this field.
+     */
+    @JsonProperty("times_received")
+    var timesReceived: Int = 0,
+
+    /**
+     * Total amount of credit earned in this field.
+     */
+    @JsonProperty("credit_in_cents")
+    var creditInCents: Int = 0
+)
+
+/**
+ * Enum of all available referral fields and its [path] in the referral document.
+ */
+enum class ReferralField(val path: String) {
+
+    CREATE_ACCOUNTS("create_accounts"),
+    SHOP("shop")
+}
